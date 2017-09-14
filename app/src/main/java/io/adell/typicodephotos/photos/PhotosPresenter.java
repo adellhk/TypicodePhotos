8b06e5f9d761bb74e1f5c2b6960056bffeb44266
Adellhk <adell.hk@gmail.com>
2017-09-13 20:27:58 -0700

    Setup photos presenter

--- app/src/main/java/io/adell/typicodephotos/photos/PhotosPresenter.java
+++ app/src/main/java/io/adell/typicodephotos/photos/PhotosPresenter.java
@@ -1,11 +1,46 @@
 package io.adell.typicodephotos.photos;
 
-import BasePresenter;
-import PhotosContract;
+import io.adell.typicodephotos.data.Photo;
+import io.adell.typicodephotos.data.TypicodeService;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * Created by Adell on 9/13/2017.
  */
 
-public class PhotosPresenter extends BasePresenter implements PhotosContract {
+public class PhotosPresenter implements PhotosContract.Presenter {
+  private final String typicodePhotoUrl = "jsonplaceholder.typicode.com/photos";
+  private TypicodeService typicodeService;
+  private PhotosContract.View photosView;
+
+  public PhotosPresenter(PhotosContract.View photosView) {
+    this.photosView = photosView;
+  }
+
+  @Override public void start() {
+    Retrofit retrofit = new Retrofit.Builder().baseUrl(typicodePhotoUrl).build();
+
+    typicodeService = retrofit.create(TypicodeService.class);
+    loadPhotos();
+  }
+
+  @Override public void loadPhotos() {
+    typicodeService.getPhotos().enqueue(new Callback<List<Photo>>() {
+      @Override public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
+        if (!response.isSuccessful()) {
+          photosView.showLoadPhotosFailure();
+        } else {
+          photosView.showPhotos(response.body());
+        }
+      }
+
+      @Override public void onFailure(Call<List<Photo>> call, Throwable t) {
+        photosView.showLoadPhotosFailure();
+      }
+    });
+  }
 }
