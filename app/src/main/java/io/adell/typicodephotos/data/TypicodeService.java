ba38af4021b27635829e7cbe706d10502190dc69
Adellhk <adell.hk@gmail.com>
2017-09-13 20:14:57 -0700

    Setup typicodeService getPhotos()

--- app/src/main/java/io/adell/typicodephotos/data/TypicodeService.java
+++ app/src/main/java/io/adell/typicodephotos/data/TypicodeService.java
@@ -1,8 +1,13 @@
 package io.adell.typicodephotos.data;
 
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by Adell on 9/13/2017.
  */
 
 public interface TypicodeService {
+  @GET("photos") Call<List<Photo>> getPhotos();
 }
