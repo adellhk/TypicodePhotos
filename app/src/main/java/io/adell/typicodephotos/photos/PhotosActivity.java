ceb4e87fb6fd349abfe864b6b7e8160d1095aec1
Adellhk <adell.hk@gmail.com>
2017-09-13 19:45:42 -0700

    Populate scaffolding

--- app/src/main/java/io/adell/typicodephotos/photos/PhotosActivity.java
+++ app/src/main/java/io/adell/typicodephotos/photos/PhotosActivity.java
@@ -1,13 +1,16 @@
 package io.adell.typicodephotos.photos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import io.adell.typicodephotos.R;
 
 public class PhotosActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_photos);
+    setContentView(R.layout.photos_activity);
+
+    PhotosFragment photosFragment = PhotosFragment.newInstance();
+    getSupportFragmentManager().beginTransaction().add(R.id.content_frame, photosFragment).commit();
   }
 }
--- app/src/main/java/io/adell/typicodephotos/photos/PhotosFragment.java
+++ app/src/main/java/io/adell/typicodephotos/photos/PhotosFragment.java
@@ -1,8 +1,31 @@
 package io.adell.typicodephotos.photos;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import io.adell.typicodephotos.R;
+
 /**
  * Created by Adell on 9/13/2017.
  */
 
-public class PhotosFragment {
+public class PhotosFragment extends Fragment {
+
+  public static PhotosFragment newInstance() {
+    PhotosFragment photosFragment = new PhotosFragment();
+    return photosFragment;
+  }
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View root = inflater.inflate(R.layout.photos_fragment, container, false);
+
+    ListView photosList = (ListView) root.findViewById(R.id.photos_list);
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
 }
